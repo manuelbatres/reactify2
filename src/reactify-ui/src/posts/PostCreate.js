b75fcdf2d31d950261846ce34ff9ef0d2a1775b0
manuelbatres <cronox.des@gmail.com>
2020-06-22 21:18:27 -0500

    Link

--- src/reactify-ui/src/posts/PostCreate.js
+++ src/reactify-ui/src/posts/PostCreate.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import 'whatwg-fetch'
 import cookie from 'react-cookies'
+import moment from 'moment'
 
 class PostCreate extends Component {
     constructor(props){
         super(props)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleDraftChange = this.handleDraftChange.bind(this)
         this.clearForm = this.clearForm.bind(this)
         this.postTitleRef = React.createRef()
         this.postContentRef = React.createRef()
@@ -52,20 +54,12 @@ class PostCreate extends Component {
 
     handleSubmit(event){
         event.preventDefault()
-        // console.log(this.state)
         let data = this.state
-        if (data['draft'] === 'on'){
-            data['draft'] = true
-        } else {
-            data['draft'] = false
-        }
-        // console.log(data)
         this.createPost(data)
     }
 
     handleInputChange(event){
         event.preventDefault()
-        console.log(event.target.name, event.target.value)
         let key = event.target.name
         let value = event.target.value
         if (key === 'title'){
@@ -76,6 +70,12 @@ class PostCreate extends Component {
         this.setState({
             [key]: value
         })
+    }
+
+    handleDraftChange(){
+      this.setState({
+        draft: !this.state.draft
+      })
     }
 
     clearForm(event){
@@ -97,12 +97,14 @@ class PostCreate extends Component {
             draft: false,
             title: null,
             content: null,
-            publish: null,
+            publish: moment(new Date()).format('YYYY-MM-DD'),
         })
         this.postTitleRef.current.focus()
     }
 
     render(){
+        const {publish} = this.state
+        let thisComp = this
         return (
             <form onSubmit={this.handleSubmit} ref={(el) => this.postCreateForm = el}>
                 <div className='form-group'>
@@ -124,14 +126,22 @@ class PostCreate extends Component {
                 </div>
                 <div className='form-group'>
                     <label for='draft'>
-                    <input type='checkbox' id='draft' name='draft' className='mr-2' onChange={this.handleInputChange}/>
+                    <input type='checkbox' checked={this.state.draft} id='draft' name='draft' className='mr-2' onChange={this.handleDraftChange}/>
                      Draft </label>
+                     <button onClick={(event)=>{event.preventDefault();this.handleDraftChange()}}>Toggle Draft</button>
                 </div>
                 <div className='form-group'>
                     <label for='publish'>Publish Date</label>
-                    <input type='date' id='publish' name='publish' className='form-control' onChange={this.handleInputChange} required='required'/>
+                    <input 
+                    type='date' 
+                    id='publish' 
+                    name='publish' 
+                    className='form-control' 
+                    onChange={this.handleInputChange}
+                     value={publish}
+                     required='required'/>
                 </div>
-                <button className='btn btn-primary'>Save</button>
+                <button type='submit' className='btn btn-primary'>Save</button>
                 <button className='btn btn-secondary' onClick={this.clearForm}>Cancel</button>
             </form>
         )
